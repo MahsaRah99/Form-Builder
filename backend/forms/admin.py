2661bde5c9f8ce27e3e09d2285d6c42d6c1f676b
mahsa <mahsarm99@gmail.com>
2024-12-13 00:07:36 +0330

    feat: add form model to admin panel

--- backend/forms/admin.py
+++ backend/forms/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Form, Question, Response
+
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 1
+    fields = (
+        "question_text",
+        "question_type",
+        "is_required",
+        "place_holder",
+        "numeric_constraint",
+    )
+    show_change_link = True
+
+
+@admin.register(Form)
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "created_at")
+    search_fields = ("title", "description")
+    inlines = [QuestionInline]
