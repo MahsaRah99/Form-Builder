d60b6b5d98dd8c093f21cf7c6b116bd24ccaab9a
mahsa <mahsarm99@gmail.com>
2024-12-12 23:16:42 +0330

    feat: add form model

--- backend/forms/models.py
+++ backend/forms/models.py
@@ -1,3 +1,15 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from .enums import NumericConstraints, QuestionTypes
+from .validators import validate_response
+
+
+class Form(models.Model):
+    title = models.CharField(_("Title"), max_length=100)
+    created_at = models.DateTimeField(_("Created at"), auto_now_add=True)
+    description = models.TextField(_("Description"), blank=True, null=True)
+
+    def __str__(self):
+        return self.title
