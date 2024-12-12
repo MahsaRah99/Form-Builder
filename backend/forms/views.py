1a134451eb57690f52c24f93005a1b71db629f39
mahsa <mahsarm99@gmail.com>
2024-12-13 00:02:59 +0330

    feat: add form retrieve view

--- backend/forms/views.py
+++ backend/forms/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response as Respnse
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Form, Question, Response
+from .serializers import FormSerializer, QuestionSerializer, ResponseSerializer
+
+
+class FormRetrieveView(generics.RetrieveAPIView):
+    serializer_class = FormSerializer
+    queryset = Form.objects.all()
